1e5c6de19006f2a6074be69c57acef41fa79f9f7
Bertrand <gaetaniusmarii@yahoo.fr>
2015-12-02 16:58:19 +0100

    Ajout script JS pour présenter la date sous le formulaire

---
+++ date.js
@@ -0,0 +1,9 @@
+var date = new Date();
+var d  = date.getDate();
+var day = (d < 10) ? '0' + d : d;
+var m = date.getMonth() + 1;
+var month = (m < 10) ? '0' + m : m;
+var yy = date.getYear();
+var year = (yy < 1000) ? yy + 1900 : yy;
+
+document.write(day + "/" + month + "/" + year);
